8aa29656d849567e722309a8cd29c916ef3515a9
juses130 <gta570@gmail.com>
2016-06-25 19:29:07 +0300

    Raziel: Update - fixing the class by the project guide instructions.

--- Coupon System/src/com/dao/interfaces/CouponDAO.java
+++ Coupon System/src/com/dao/interfaces/CouponDAO.java
@@ -5,10 +5,10 @@ import com.javabeans.*;
 
 public interface CouponDAO {
 
-	public void createCoupon(Coupon newCoupon);
+	public long createCoupon(Coupon newCoupon);
 	public void removeCoupon(Coupon remCoupon);
 	public void updateCoupon(Coupon upCoupon);
-	public Coupon getCoupon();
+	public Coupon getCoupon(long id);
 	public Collection<Coupon> getAllCoupon();
 	public Collection<Coupon> getCouponByType();
 }
